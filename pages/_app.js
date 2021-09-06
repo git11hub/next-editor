d3d3451d2b072b8db730b0388b9536d232845b73
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-09-06 19:14:38 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import './RichText.css';
+import '../node_modules/draft-js/dist/Draft.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
